e0923f5e23c512c3a9a93bf5106ae5bcdd38aba3
dcasco02 <dcasco@unitec.edu>
2022-05-06 13:35:59 -0600

    Mnu

--- src/diegocasco_lab3p2/DiegoCasco_Lab3P2.java
+++ src/diegocasco_lab3p2/DiegoCasco_Lab3P2.java
@@ -5,6 +5,10 @@
  */
 package diegocasco_lab3p2;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author dcasc
@@ -15,7 +19,23 @@ public class DiegoCasco_Lab3P2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        ArrayList propiedad=new ArrayList();
+        ArrayList comprador=new ArrayList();
+        Scanner lea=new Scanner(System.in);
+        int opcion=69;
+        while(opcion!=0){
+           opcion=Integer.parseInt(
+           JOptionPane.showInputDialog("0-Salida\n"
+                   + "1-Agregar Propiedad\n"
+                   + "2-Agregar Comprador\n"
+                   + "3-Modificar Propiedades\n"
+                   + "4-Eliminar Propiedades\n"
+                   + "5-Comprar Propiedades\n"
+                   + ""));
+           if(opcion==1){
+               
+           }
+        }
     }
     
 }
