e67e341582b70d1160f60b2a132a55bd1ef9e39c
dcasco02 <dcasco@unitec.edu>
2022-05-06 13:17:01 -0600

    first commit

---
+++ src/diegocasco_lab3p2/DiegoCasco_Lab3P2.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package diegocasco_lab3p2;
+
+/**
+ *
+ * @author dcasc
+ */
+public class DiegoCasco_Lab3P2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
